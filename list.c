3cfc263c3cc7d596a1e02718ef8128742b4e44b5
Nicholas Ming Li <nmli@unix4.csc.calpoly.edu>
2023-11-15 23:41:20 -0800

    "commented some steps to list.c"

--- list.c
+++ list.c
@@ -1,1 +1,6 @@
 #include "tarstuff.h"
+
+// when verbose is not set, read each header section at name_offset and print the file name
+// when verbose is set, read each header at name_offset for name, mode_offset for permissions and convert to
+// drwx---rw- format, and so on for the rquired feilds, then pad with spaces to the proper length of each feild and print
+//
