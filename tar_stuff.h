53a91a4466384e8c2264e7265b943c4d107b4c78
Nicholas Ming Li <nmli@unix4.csc.calpoly.edu>
2023-11-15 23:30:41 -0800

    "added header struct to tar_stuff.h and created macros for the offsets of certain feilds in the header"

--- tar_stuff.h
+++ tar_stuff.h
@@ -6,11 +6,34 @@
 
 #define ON 1
 #define OFF 0
-
+#define name_offset 0
+#define mode_offset 100
+#define gname_offset 297
+#define size_offset 124
+#define mtime_offset 136
 
 typedef struct{
     int c,t,x,v,f,S;
 }Options;
+
+typedef struct {
+char name[100];
+char mode[8];
+char uid[8];
+char gid[8];
+char size[12];
+char mtime[12];
+char chksum[8];
+char typeflag;
+char linkname[100];
+char magic[6];  
+char version[2];
+char uname[32];
+char gname[32];
+char devmajor[8];
+char devminor[8];
+char prefix[155];
+}Header;
 
 
 Options *check_options(char *options);
