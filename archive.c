a01b654dbec7a0b1def196df08ceec5642c4eb8c
Lukas Chou <lukaschou21@icloud.com>
2023-11-20 20:14:56 -0800

    reconfigured file traverse

--- archive.c
+++ archive.c
@@ -1,78 +1,42 @@
 #include "tar_stuff.h"
-/*
-void process_roots(char *name, char*name, Options *opts) {
+
+void traverse_files(char *path, Options *opts) {
 	DIR *dir;
-	if ((dir = openddir(".")	
-
-	
-	if (chdir(name) == -1) {
-		perror("chdir");
-		exit(EXIT_FAILURE);
-	}
-	
-
-
-}
-*/
-
-void traverse_files(char *path, char *name, Options *opts) {
-	DIR *cur_dir;
-	struct dirent *cur_ent;
+	struct dirent *ent;
 	struct stat sb;
-	char cur_path[PATH_MAX];
-	char cwd[PATH_MAX*2];
+	char fullpath[PATH_MAX];
 	
-	/* change dir to relative path (name)  */
-	if (chdir(name) == -1) {
-		perror("chdir");
-		exit(EXIT_FAILURE);
+	if (opts->v) {
+		printf("%s/\n", path);
 	}
 
 	/* open current directory */
-	if ((cur_dir = opendir(".")) == NULL) {
+	if ((dir = opendir(path)) == NULL) {
 		perror("opendir");
-		exit(EXIT_FAILURE);
+		exit(EXIT_FAILURE); //prob don't wan't to exit here.
 	}
 	
 	/* iterate through current's entries */
 	/* what do I do if I can't read an entry? */
-	while ((cur_ent = readdir(cur_dir)) != NULL) {
+	while ((ent = readdir(dir)) != NULL) {
 		/* skip over '.' and '..' entries  */
-		if (strcmp(cur_ent->d_name, ".") != 0 && strcmp(cur_ent->d_name, "..") != 0) {
-			if (lstat(cur_ent->d_name, &sb) == -1) {
-				perror("lstat");
-				exit(EXIT_FAILURE);
-			}
-			
-			/* add current entry name to path */
-			strcpy(cur_path, path);
-			strcat(cur_path, "/");
-			strcat(cur_path, cur_ent->d_name);
-	
-			/* if verbose option on, print paths as we go */
-			if (opts->v){
-				printf("%s\n", cur_path);
-			}
+		if (strcmp(ent->d_name, ".") != 0 && strcmp(ent->d_name, "..") != 0) {
+				snprintf(fullpath, PATH_MAX, "%s/%s", path, ent->d_name);
 
-			/* if entry is directory, not symlink */
-			if (S_ISDIR(sb.st_mode) && !S_ISLNK(sb.st_mode)) {
-				/* save current working directory before recursing  */
-				if (getcwd(cwd, PATH_MAX*2) == NULL) {
-					perror("cwd");
-					exit(EXIT_FAILURE);
+				if (lstat(fullpath, &sb) == -1) {
+					perror("lstat");
+					closedir(dir);
+					exit(EXIT_FAILURE); //prob shouldn't exit here
 				}
-				
-				/* call self on directory */	
-				traverse_files(cur_path, cur_ent->d_name, opts);
-				
-				/* change back to parent */
-				if (chdir(cwd) == -1) {
-					perror("chdir to cwd");
-					exit(EXIT_FAILURE);
+
+				if (S_ISDIR(sb.st_mode) && !S_ISLNK(sb.st_mode)) {
+					traverse_files(fullpath, opts);
+				} else {
+					if (opts->v) {
+						printf("%s\n", fullpath);
+					}
 				}
 			}
-		}
 	}
-	closedir(cur_dir);
-	/* will this chdir back to root? */
+	closedir(dir);
 }
