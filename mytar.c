5deceb9fee436729a8a596b2a162c007e26a9d06
Lukas Chou <lukaschou21@icloud.com>
2023-11-26 01:33:32 -0800

    comments, list stuff

--- mytar.c
+++ mytar.c
@@ -16,40 +16,70 @@ int main(int argc, char *argv[]) {
 
 	/* create */
 	if (opts->c) {
+        /* if there aren't enough args */
 		if (argc < 4) {
 			fprintf(stderr, "usage: mytar [txvS]cf tarfile [ path [ ... ] ]\n");
 			exit(EXIT_FAILURE);
 		}
+        
+        /* malloc space for path arg */
 		if ((path = malloc(255)) == NULL) {
-			perror("malloc on path");
+			perror("failed to malloc path");
 			exit(EXIT_FAILURE);	
 		}
+
+        /* open the tarfile arg, save its file descriptor */
 		if ((tarfile = open(argv[2], O_RDWR | O_CREAT | O_TRUNC, 
 			S_IRWXU | S_IRWXG)) == -1) {
-			perror("open");
+			perror("failed to open tarfile");
 			exit(EXIT_FAILURE);
 		}
+
 		/* traverse every file in path argument */
 		for (i = 3; i < argc; i++) {
+            /* if initial path is too big, skip to next one */
 			if (strlen(argv[i]) > MAX_PATH) {
-				fprintf(stderr, "pathname over 256 characters");
-				exit(EXIT_FAILURE);
+				printf("pathname over 256 characters: %s\n", argv[i]);
+				continue;
 			}
+            /* save path and begin write with it */
 			pathlen = strlen(argv[i]) + 1;
 			memmove(path, argv[i], pathlen);
 			traverse_files(path, opts, tarfile);
 		}
-		write_end(tarfile);
-        close(tarfile);     
+		write_end(tarfile); /* write last 2 blocks of NULLS */
+        close(tarfile); /* close tarfile */     
 	}	
 
+    /* table of contents */
 	if (opts->t) {
+        int num_paths = 0; /* number of optional paths */
+        char **paths = NULL; /* array of path strings */
+
+        /* if there are optional paths */
+        if (argc > 3) {
+            num_paths = argc - 3;
+            if ((paths = malloc(num_paths * sizeof(char *))) == NULL) {
+                perror("failed to malloc paths");
+                exit(EXIT_FAILURE);
+            }
+            /* fill paths array */
+            for (i = 3; i < argc; i++) {
+                paths[i - 3] = argv[i];
+                printf("%s\n", paths[i-3]);
+            }
+        }
+        
+        /* open tarfile, save its file descriptor */
 		if ((tarfile = open(argv[2], O_RDONLY)) == -1) {
-			perror("open");
+			perror("failed to open tarfile");
 			exit(EXIT_FAILURE);
 		}
+        
+        /* read, print contents of tarfile */
 		contents(tarfile, opts);
-		close(tarfile);
+        free(paths); /* free paths */
+		close(tarfile); /* close tarfile */
 	}
 	return 0;
 }
