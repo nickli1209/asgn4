fb54b90a6245cea9206ad98c933913f840fb843a
Lukas Chou <lukaschou21@icloud.com>
2023-11-27 22:56:11 -0800

    long lines

--- list.c
+++ list.c
@@ -7,7 +7,7 @@ void contents(int tarfile, Options *opts,char ** pathList) {
  	uint8_t buf[BLOCK_SIZE]; /* buffer for block of tarfile */
  	char fullpath[MAX_PATH]; /* for concatenating name and prefix */
   	unsigned long size; /* for converting from octal to int */
-  	int offset, bytes_read; /* offset in tarfile, bytes read for error checking */
+  	int offset, bytes_read; /* offset in tarfile  */
   	Header *header; /* header struct */
   	uint8_t check[BLOCK_SIZE]; /* check for 2 NULL blocks at end */
     int prntpths; /* flag for printing paths */
@@ -54,7 +54,7 @@ void contents(int tarfile, Options *opts,char ** pathList) {
                 }
             } 
         }
-		
+	    /* all these prints are a bit messy. oops */	
         /* if prntpths ON, print everything */
         if (prntpths) {
             /* if verbose, print additional info */
